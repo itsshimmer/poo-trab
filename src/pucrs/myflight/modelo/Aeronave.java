6b5f3d4e9cbbc271bbd8fa87d17e58320a9860ec
eduardo a soares <eduardo.andre.soares@icloud.com>
2019-08-19 00:11:31 -0300

    Acrescentei alguns trechos de código na classe Aeronave

--- src/pucrs/myflight/modelo/Aeronave.java
+++ src/pucrs/myflight/modelo/Aeronave.java
@@ -3,10 +3,12 @@ package pucrs.myflight.modelo;
 public class Aeronave {
 	private String codigo;
 	private String descricao;
+	private int capacidade;
 	
-	public Aeronave(String codigo, String descricao) {
+	public Aeronave(String codigo, String descricao, int capacidade) {
 		this.codigo = codigo;
 		this.descricao = descricao;
+		this.capacidade = capacidade;
 	}
 	
 	public String getCodigo() {
@@ -15,5 +17,9 @@ public class Aeronave {
 	
 	public String getDescricao() {
 		return descricao;
+	}
+
+	public int getCapacidade(){
+		return capacidade;
 	}
 }
