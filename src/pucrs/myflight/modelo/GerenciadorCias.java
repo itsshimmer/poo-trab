95c0fcb7b820ad09875539ca45a98774ea968851
arczyx <34427420+arczyx@users.noreply.github.com>
2019-08-21 18:48:19 -0300

    Atualizado conforme aula

--- src/pucrs/myflight/modelo/GerenciadorCias.java
+++ src/pucrs/myflight/modelo/GerenciadorCias.java
@@ -8,4 +8,22 @@ public class GerenciadorCias {
 	public GerenciadorCias() {
 		empresas = new ArrayList<>();
 	}
+
+	public void inserir(String cod, String nome) {
+		empresas.add(new CiaAerea(cod, nome));
+	}
+
+	public ArrayList<CiaAerea> getCias() {
+		return empresas;
+	}
+
+	public CiaAerea pesquisar(String codigo) {
+		for (CiaAerea cia: empresas) {
+			if(cia.getCodigo().compareTo(codigo)==0) {
+				return cia;
+			}
+		}
+		return null;
+	}
+
 }
